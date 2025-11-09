bb53f98ca8dcaf3f51de1d5ced8798d13bd8c4cf
KeylinRojas15 <kerojasob@est.utn.ac.cr>
2025-11-08 19:55:12 -0600

    base

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+    //smooth ScrollSmoother
+    ScrollSmoother.create({
+        smooth: 2,
+        effects: true,
+    });
+
+
+    ////scrolling el robot se desplaza hacia la izquierda
+    gsap.to("#robot", {
+        scrollTrigger: {
+            trigger: "#seccion2",
+            start: "top bottom",
+            end: "bottom bottom",
+            scrub: 1,
+            
+        },
+        y: "110%",
+        x: "-25%",
+        ease: "power1.inOut",
+        immediateRender: false,
+    });
+
+        
+    gsap.to("#robot", {
+        scrollTrigger: {
+            trigger: "#seccion3",
+            start: "top bottom",
+            end: "center center",
+            scrub: 1,
+        },
+        y: "210%",
+        x: "2%",
+        ease: "power1.inOut",
+        immediateRender: false,
+    });
+
+});
