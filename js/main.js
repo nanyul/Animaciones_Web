384f370378d7d429cd49acd307b99dfd8f5ec8a1
KeylinRojas15 <kerojasob@est.utn.ac.cr>
2025-11-10 19:11:52 -0600

    pantalla 1024px

--- js/main.js
+++ js/main.js
@@ -10,50 +10,104 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 
     ////scrolling el robot se desplaza hacia la izquierda
-    gsap.to("#robot", {
-        scrollTrigger: {
-            trigger: "#seccion2",
-            start: "top bottom",
-            end: "bottom bottom",
-            scrub: 1,
+    ScrollTrigger.matchMedia({
+        // Desktop
+        "(min-width: 1024px)": function () {
+            gsap.to("#robot", {
+                scrollTrigger: {
+                    trigger: "#seccion2",
+                    start: "top bottom",
+                    end: "bottom bottom",
+                    scrub: 1,
 
+                },
+                y: "110%",
+                x: "-25%",
+                ease: "power1.inOut",
+                immediateRender: false,
+            });
         },
-        y: "110%",
-        x: "-25%",
-        ease: "power1.inOut",
-        immediateRender: false,
+
+        // Mobile
+        "(max-width: 1023px)": function () {
+            const robot = document.querySelector("#robot");
+            const imageHeight = robot.offsetHeight;
+            gsap.to("#robot", {
+                scrollTrigger: {
+                    trigger: "#seccion2",
+                    start: "top bottom",
+                    end: "bottom bottom",
+                    scrub: 1,
+
+                },
+                y: "200%",
+                x: "-55%",
+                ease: "power1.inOut",
+                immediateRender: false,
+            });
+        },
     });
 
 
-    gsap.to("#robot", {
-        scrollTrigger: {
-            trigger: "#seccion3",
-            start: "top bottom",
-            end: "center center",
-            scrub: 1,
+    //seccion 3 y 5
+    ScrollTrigger.matchMedia({
+        // Desktop
+        "(min-width: 1024px)": function () {
+            gsap.to("#robot", {
+                scrollTrigger: {
+                    trigger: "#seccion3",
+                    start: "top bottom",
+                    end: "center center",
+                    scrub: 1,
+                },
+                y: "216.5%",
+                x: "2%",
+                ease: "power1.inOut",
+                immediateRender: false,
+            });
+
+            gsap.to("#robot", {
+                scrollTrigger: {
+                    trigger: "#seccion5",
+                    start: "top bottom",
+                    end: "center center",
+                    scrub: 1,
+                },
+                y: "420%",
+                x: "37%",
+                ease: "power1.inOut",
+                immediateRender: false,
+            });
         },
-        y: "216.5%",
-        x: "2%",
-        ease: "power1.inOut",
-        immediateRender: false,
+
+        // Mobile
+        "(max-width: 1023px)": function () {
+            gsap.to("#robot", {
+                scrollTrigger: {
+                    trigger: "#seccion3",
+                    start: "top bottom",
+                    end: "center center",
+                    scrub: 1,
+                },
+                y: "385%",
+                x: "3%",
+                ease: "power1.inOut",
+                immediateRender: false,
+            });
+
+            gsap.to("#robot", {
+                scrollTrigger: {
+                    trigger: "#seccion5",
+                    start: "top bottom",
+                    end: "center center",
+                    scrub: 1,
+                },
+                y: "600%",
+                x: "55%",
+                ease: "power1.inOut",
+                immediateRender: false,
+            });
+        }
     });
 
-
-
-
-    gsap.to("#robot", {
-        scrollTrigger: {
-            trigger: "#seccion5",
-            start: "top bottom",
-            end: "center center",
-            scrub: 1,
-            
-        },
-        y: "420%",
-        x: "37%",
-        ease: "power1.inOut",
-        immediateRender: false,
-    });
-    
 });
-    
